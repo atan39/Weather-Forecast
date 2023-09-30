62b84c48c99260ac302cebc4d99f7f03ac558189
Ashley Tan <ashleytan39@gmail.com>
2023-09-30 01:42:29 -0500

    finished code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Weather-Forecast
+
+A website that allows you to check the current and 5 day weather of any city. 
+
+## Pictures
+![image](https://github.com/atan39/Weather-Forecast/assets/126987766/970c0ea2-1f3c-4a89-8589-6d1a7533c5c4)
+
+![image](https://github.com/atan39/Weather-Forecast/assets/126987766/fc65192c-24f5-45e6-98a8-77b39784d161)
--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,73 @@
 const apiKey = "bc93c8c21c3f53980eab1fad1d379249";
 
-// Build a URL with your api key
-const cityName = 'Chicago';
-const stateCode = 'IL';
-const countryCode = 'US';
-const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=20&appid=${apiKey}`;
+document.getElementById("search-form").addEventListener("submit", handleFormSubmit);
 
-// Call the URL using fetch()
-fetch(apiUrl)
-    .then(function (response){
-        return response.json();
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const city = document.getElementById("city").value.trim();
+  if (city === "") return;
+
+  getCityLocation(city)
+    .then(({ lat, lon, name }) => {
+      updateHistory(name, lat, lon);
+      fetchWeatherData(lat, lon);
     })
-    .then(function (data){
-        console.log(data)
-        const city = data[0];
-        // city.lat
-        const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=${apiKey}`;
-
-        // here
-        fetch(weatherUrl)
-        .then(function (response){
-            return response.json();
-        })
-        .then(function (data){
-            console.log(data)
-        })
+    .catch(() => {
+      console.log("No results found.");
     });
+
+  document.getElementById("city").value = "";
+}
+
+async function getCityLocation(city) {
+  const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`);
+  const [location] = await response.json();
+  if (!location) throw new Error("No results found.");
+  return location;
+}
+
+async function fetchWeatherData(lat, lon) {
+  const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+  const weatherData = await weatherResponse.json();
+  const temperature = ((weatherData.main.temp - 273.15) * 9 / 5) + 32;
+  document.getElementById("current-weather-data").textContent = `Temperature: ${temperature.toFixed(2)}°F`;
+
+  const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+  const forecastStructure = await forecastResponse.json();
+
+  displayForecast(forecastStructure.list);
+}
+
+function displayForecast(forecastList) {
+  const forecastInfo = document.getElementById("forecast-data");
+  forecastInfo.innerHTML = "";
+
+  forecastList
+    .filter((_, index) => index % 8 === 0)
+    .forEach(forecastItem => {
+      const date = new Date(forecastItem.dt_txt);
+      const temperature = ((forecastItem.main.temp - 273.15) * 9 / 5) + 32;
+
+      const forecastElement = createForecastElement(date.toLocaleDateString(), temperature.toFixed(2));
+      forecastInfo.appendChild(forecastElement);
+    });
+}
+
+function createForecastElement(date, temperature) {
+  const forecastElement = document.createElement("div");
+  forecastElement.innerHTML = `
+    <p>Date: ${date}</p>
+    <p>Temperature: ${temperature}°F</p>
+  `;
+  return forecastElement;
+}
+
+function updateHistory(cityName, lat, lon) {
+  const history = document.getElementById("history-data");
+  const listedData = document.createElement("li");
+  listedData.textContent = cityName;
+  listedData.addEventListener("click", () => {
+    fetchWeatherData(lat, lon);
+  });
+  history.appendChild(listedData);
+}
